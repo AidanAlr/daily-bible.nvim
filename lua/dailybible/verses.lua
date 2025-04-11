b0df51a9224e3df890b7f0dc9e789f54c55b4eb3
Aidan Alrawi <112656616+AidanAlr@users.noreply.github.com>
2025-04-11 01:41:23 -0400

    additional verses

--- lua/dailybible/verses.lua
+++ lua/dailybible/verses.lua
@@ -171,4 +171,322 @@ return {
 		verse = "Proverbs 3:27",
 		text = "Do not withhold good from those to whom it is due, when it is in your power to act.",
 	},
+	{
+		verse = "Psalm 23:4",
+		text = "Even though I walk through the darkest valley, I will fear no evil, for you are with me; your rod and your staff, they comfort me.",
+	},
+	{
+		verse = "Psalm 23:6",
+		text = "Surely your goodness and love will follow me all the days of my life, and I will dwell in the house of the LORD forever.",
+	},
+	{
+		verse = "Psalm 23:5",
+		text = "You prepare a table before me in the presence of my enemies. You anoint my head with oil; my cup overflows.",
+	},
+	{ verse = "Psalm 23:1", text = "The LORD is my shepherd, I lack nothing." },
+	{ verse = "Psalm 23:3", text = "he refreshes my soul. He guides me along the right paths for his name’s sake." },
+	{ verse = "Psalm 23:2", text = "He makes me lie down in green pastures, he leads me beside quiet waters," },
+	{
+		verse = "Jeremiah 29:11",
+		text = "'For I know the plans I have for you,' declares the Lord, 'plans to prosper you and not to harm you, plans to give you hope and a future.'",
+	},
+	{
+		verse = "John 3:16",
+		text = "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.",
+	},
+	{ verse = "Psalm 91:11", text = "For he will command his angels concerning you to guard you in all your ways;" },
+	{
+		verse = "Psalm 91:1",
+		text = "Whoever dwells in the shelter of the Most High will rest in the shadow of the Almighty.",
+	},
+	{
+		verse = "Psalm 91:2",
+		text = "I will say of the Lord, 'He is my refuge and my fortress, my God, in whom I trust.'",
+	},
+	{
+		verse = "Psalm 91:4",
+		text = "He will cover you with his feathers, and under his wings you will find refuge; his faithfulness will be your shield and rampart.",
+	},
+	{
+		verse = "Psalm 91:12",
+		text = "they will lift you up in their hands, so that you will not strike your foot against a stone.",
+	},
+	{
+		verse = "Psalm 91:15",
+		text = "He will call on me, and I will answer him; I will be with him in trouble, I will deliver him and honor him.",
+	},
+	{ verse = "Psalm 91:16", text = "'With long life I will satisfy him and show him my salvation.'" },
+	{
+		verse = "Psalm 91:14",
+		text = "'Because he loves me,' says the Lord, 'I will rescue him; I will protect him, for he acknowledges my name.'",
+	},
+	{
+		verse = "Psalm 91:7",
+		text = "A thousand may fall at your side, ten thousand at your right hand, but it will not come near you.",
+	},
+	{ verse = "Psalm 91:10", text = "no harm will overtake you, no disaster will come near your tent." },
+	{ verse = "Psalm 91:9", text = "If you say, 'The Lord is my refuge,' and you make the Most High your dwelling," },
+	{ verse = "Psalm 91:8", text = "You will only observe with your eyes and see the punishment of the wicked." },
+	{ verse = "Psalm 91:5", text = "You will not fear the terror of night, nor the arrow that flies by day," },
+	{
+		verse = "Psalm 91:13",
+		text = "You will tread on the lion and the cobra; you will trample the great lion and the serpent.",
+	},
+	{
+		verse = "Psalm 91:3",
+		text = "Surely he will save you from the fowler’s snare and from the deadly pestilence.",
+	},
+	{
+		verse = "Psalm 91:6",
+		text = "nor the pestilence that stalks in the darkness, nor the plague that destroys at midday.",
+	},
+	{
+		verse = "Joshua 1:9",
+		text = "'Have I not commanded you? Be strong and courageous. Do not be afraid; do not be discouraged, for the LORD your God will be with you wherever you go.'",
+	},
+	{
+		verse = "Isaiah 41:10",
+		text = "So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand.",
+	},
+	{
+		verse = "Romans 8:28",
+		text = "And we know that in all things God works for the good of those who love him, who have been called according to his purpose.",
+	},
+	{
+		verse = "John 14:6",
+		text = "Jesus answered, 'I am the way and the truth and the life. No one comes to the Father except through me.'",
+	},
+	{
+		verse = "Romans 12:2",
+		text = "Do not conform to the pattern of this world, but be transformed by the renewing of your mind. Then you will be able to test and approve what God’s will ishis good, pleasing and perfect will.",
+	},
+	{
+		verse = "Matthew 6:33",
+		text = "But seek first his kingdom and his righteousness, and all these things will be given to you as well.",
+	},
+	{
+		verse = "Ephesians 6:12",
+		text = "For our struggle is not against flesh and blood, but against the rulers, against the authorities, against the powers of this dark world and against the spiritual forces of evil in the heavenly realms.",
+	},
+	{
+		verse = "Philippians 4:6",
+		text = "Do not be anxious about anything, but in every situation, by prayer and petition, with thanksgiving, present your requests to God. [Read Curtis Chang’s analysis of this verse and chapter.]",
+	},
+	{ verse = "Philippians 4:13", text = "I can do all this through him who gives me strength." },
+	{
+		verse = "Philippians 4:7",
+		text = "And the peace of God, which transcends all understanding, will guard your hearts and your minds in Christ Jesus.",
+	},
+	{
+		verse = "John 16:33",
+		text = "'I have told you these things, so that in me you may have peace. In this world you will have trouble. But take heart! I have overcome the world.'",
+	},
+	{
+		verse = "Philippians 4:8",
+		text = "Finally, brothers and sisters, whatever is true, whatever is noble, whatever is right, whatever is pure, whatever is lovely, whatever is admirableif anything is excellent or praiseworthythink about such things.",
+	},
+	{
+		verse = "Isaiah 40:31",
+		text = "but those who hope in the LORD will renew their strength. They will soar on wings like eagles; they will run and not grow weary, they will walk and not be faint.",
+	},
+	{
+		verse = "Proverbs 3:5",
+		text = "Trust in the LORD with all your heart and lean not on your own understanding; [See Megan Marshman’s devotional on this verse.]",
+	},
+	{ verse = "Proverbs 3:6", text = "in all your ways submit to him, and he will make your paths straight." },
+	{ verse = "1 Peter 5:7", text = "Cast all your anxiety on him because he cares for you." },
+	{
+		verse = "Isaiah 54:17",
+		text = "'no weapon forged against you will prevail, and you will refute every tongue that accuses you. This is the heritage of the servants of the LORD, and this is their vindication from me,' declares the LORD.",
+	},
+	{
+		verse = "2 Timothy 1:7",
+		text = "For the Spirit God gave us does not make us timid, but gives us power, love and self-discipline.",
+	},
+	{
+		verse = "2 Corinthians 5:17",
+		text = "Therefore, if anyone is in Christ, the new creation has come: The old has gone, the new is here!",
+	},
+	{ verse = "Matthew 11:28", text = "'Come to me, all you who are weary and burdened, and I will give you rest.'" },
+	{
+		verse = "John 10:10",
+		text = "The thief comes only to steal and kill and destroy; I have come that they may have life, and have it to the full.",
+	},
+	{
+		verse = "Matthew 28:20",
+		text = "'and teaching them to obey everything I have commanded you. And surely I am with you always, to the very end of the age.'",
+	},
+	{
+		verse = "1 Corinthians 13:4",
+		text = "Love is patient, love is kind. It does not envy, it does not boast, it is not proud. [See Scot McKnight’s commentary on this chapter and verse.]",
+	},
+	{
+		verse = "2 Corinthians 12:9",
+		text = "But he said to me, 'My grace is sufficient for you, for my power is made perfect in weakness.' Therefore I will boast all the more gladly about my weaknesses, so that Christ’s power may rest on me.",
+	},
+	{
+		verse = "Galatians 5:22",
+		text = "But the fruit of the Spirit is love, joy, peace, forbearance, kindness, goodness, faithfulness,",
+	},
+	{
+		verse = "John 14:27",
+		text = "Peace I leave with you; my peace I give you. I do not give to you as the world gives. Do not let your hearts be troubled and do not be afraid.",
+	},
+	{ verse = "Psalm 121:8", text = "the Lord will watch over your coming and going both now and forevermore." },
+	{ verse = "1 Corinthians 13:7", text = "It always protects, always trusts, always hopes, always perseveres." },
+	{
+		verse = "Matthew 28:19",
+		text = "Therefore go and make disciples of all nations, baptizing them in the name of the Father and of the Son and of the Holy Spirit,",
+	},
+	{ verse = "Psalm 121:7", text = "The Lord will keep you from all harmhe will watch over your life;" },
+	{ verse = "Psalm 121:2", text = "My help comes from the Lord, the Maker of heaven and earth." },
+	{
+		verse = "Psalm 1:3",
+		text = "That person is like a tree planted by streams of water, which yields its fruit in season and whose leaf does not wither whatever they do prospers.",
+	},
+	{
+		verse = "Romans 12:1",
+		text = "Therefore, I urge you, brothers and sisters, in view of God’s mercy, to offer your bodies as a living sacrifice, holy and pleasing to Godthis is your true and proper worship.",
+	},
+	{ verse = "Psalm 121:3", text = "He will not let your foot sliphe who watches over you will not slumber;" },
+	{ verse = "Psalm 121:1", text = "I lift up my eyes to the mountainswhere does my help come from?" },
+	{ verse = "Psalm 121:4", text = "indeed, he who watches over Israel will neither slumber nor sleep." },
+	{
+		verse = "1 Corinthians 13:5",
+		text = "It does not dishonor others, it is not self-seeking, it is not easily angered, it keeps no record of wrongs.",
+	},
+	{ verse = "Psalm 121:5", text = "The Lord watches over youthe Lord is your shade at your right hand;" },
+	{ verse = "Psalm 121:6", text = "the sun will not harm you by day, nor the moon by night." },
+	{
+		verse = "2 Timothy 3:16",
+		text = "All Scripture is God-breathed and is useful for teaching, rebuking, correcting and training in righteousness,",
+	},
+	{ verse = "Galatians 5:23", text = "gentleness and self-control. Against such things there is no law." },
+	{
+		verse = "Psalm 1:2",
+		text = "but whose delight is in the law of the LORD, and who meditates on his law day and night.",
+	},
+	{
+		verse = "Psalm 1:1",
+		text = "Blessed is the one who does not walk in step with the wicked or stand in the way that sinners take or sit in the company of mockers,",
+	},
+	{
+		verse = "Genesis 1:27",
+		text = "So God created mankind in his own image, in the image of God he created them; male and female he created them.",
+	},
+	{ verse = "1 Corinthians 13:6", text = "Love does not delight in evil but rejoices with the truth." },
+	{
+		verse = "1 John 1:9",
+		text = "If we confess our sins, he is faithful and just and will forgive us our sins and purify us from all unrighteousness.",
+	},
+	{
+		verse = "1 Thessalonians 5:18",
+		text = "give thanks in all circumstances; for this is God’s will for you in Christ Jesus.",
+	},
+	{
+		verse = "Psalm 46:10",
+		text = "He says, 'Be still, and know that I am God; I will be exalted among the nations, I will be exalted in the earth.'",
+	},
+	{
+		verse = "2 Corinthians 10:5",
+		text = "We demolish arguments and every pretension that sets itself up against the knowledge of God, and we take captive every thought to make it obedient to Christ.",
+	},
+	{
+		verse = "2 Chronicles 7:14",
+		text = "if my people, who are called by my name, will humble themselves and pray and seek my face and turn from their wicked ways, then I will hear from heaven, and I will forgive their sin and will heal their land.",
+	},
+	{
+		verse = "1 Peter 2:9",
+		text = "But you are a chosen people, a royal priesthood, a holy nation, God’s special possession, that you may declare the praises of him who called you out of darkness into his wonderful light.",
+	},
+	{
+		verse = "Ephesians 2:10",
+		text = "For we are God’s handiwork, created in Christ Jesus to do good works, which God prepared in advance for us to do.",
+	},
+	{
+		verse = "Psalm 139:14",
+		text = "I praise you because I am fearfully and wonderfully made; your works are wonderful, I know that full well.",
+	},
+	{
+		verse = "Hebrews 4:12",
+		text = "For the word of God is alive and active. Sharper than any double-edged sword, it penetrates even to dividing soul and spirit, joints and marrow; it judges the thoughts and attitudes of the heart.",
+	},
+	{
+		verse = "Ephesians 3:20",
+		text = "Now to him who is able to do immeasurably more than all we ask or imagine, according to his power that is at work within us,",
+	},
+	{
+		verse = "Matthew 11:29",
+		text = "Take my yoke upon you and learn from me, for I am gentle and humble in heart, and you will find rest for your souls.",
+	},
+	{
+		verse = "1 Corinthians 13:8",
+		text = "Love never fails. But where there are prophecies, they will cease; where there are tongues, they will be stilled; where there is knowledge, it will pass away.",
+	},
+	{
+		verse = "Ephesians 2:8",
+		text = "For it is by grace you have been saved, through faithand this is not from yourselves, it is the gift of God",
+	},
+	{
+		verse = "Ephesians 6:11",
+		text = "Put on the full armor of God, so that you can take your stand against the devil’s schemes.",
+	},
+	{
+		verse = "Isaiah 53:5",
+		text = "But he was pierced for our transgressions, he was crushed for our iniquities; the punishment that brought us peace was on him, and by his wounds we are healed.",
+	},
+	{
+		verse = "Psalm 1:6",
+		text = "For the Lord watches over the way of the righteous, but the way of the wicked leads to destruction.",
+	},
+	{
+		verse = "1 Corinthians 10:13",
+		text = "No temptation has overtaken you except what is common to mankind. And God is faithful; he will not let you be tempted beyond what you can bear. But when you are tempted, he will also provide a way out so that you can endure it.",
+	},
+	{ verse = "Psalm 1:4", text = "Not so the wicked! They are like chaff that the wind blows away." },
+	{
+		verse = "Psalm 100:4",
+		text = "Enter his gates with thanksgiving and his courts with praise; give thanks to him and praise his name.",
+	},
+	{
+		verse = "Psalm 1:5",
+		text = "Therefore the wicked will not stand in the judgment, nor sinners in the assembly of the righteous.",
+	},
+	{
+		verse = "Romans 5:8",
+		text = "But God demonstrates his own love for us in this: While we were still sinners, Christ died for us.",
+	},
+	{
+		verse = "1 Peter 5:8",
+		text = "Be alert and of sober mind. Your enemy the devil prowls around like a roaring lion looking for someone to devour.",
+	},
+	{
+		verse = "Galatians 6:9",
+		text = "Let us not become weary in doing good, for at the proper time we will reap a harvest if we do not give up.",
+	},
+	{
+		verse = "Hebrews 12:2",
+		text = "fixing our eyes on Jesus, the pioneer and perfecter of faith. For the joy set before him he endured the cross, scorning its shame, and sat down at the right hand of the throne of God.",
+	},
+	{
+		verse = "Colossians 3:23",
+		text = "Whatever you do, work at it with all your heart, as working for the Lord, not for human masters,",
+	},
+	{
+		verse = "Hebrews 11:1",
+		text = "Now faith is confidence in what we hope for and assurance about what we do not see.",
+	},
+	{
+		verse = "Philippians 4:19",
+		text = "And my God will meet all your needs according to the riches of his glory in Christ Jesus.",
+	},
+	{ verse = "Romans 3:23", text = "for all have sinned and fall short of the glory of God," },
+	{
+		verse = "Acts 1:8",
+		text = "'But you will receive power when the Holy Spirit comes on you; and you will be my witnesses in Jerusalem, and in all Judea and Samaria, and to the ends of the earth.'",
+	},
+	{
+		verse = "Genesis 1:26",
+		text = "Then God said, 'Let us make mankind in our image, in our likeness, so that they may rule over the fish in the sea and the birds in the sky, over the livestock and all the wild animals, and over all the creatures that move along the ground.'",
+	},
 }
